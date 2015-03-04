c5bc3c1578de29c6ac3a51fee1bd52e904a55ce9
shiyangliu <lsyang@mit.edu>
2015-03-03 23:44:12 -0500

    working for some cases

--- parta.py
+++ parta.py
@@ -2,24 +2,25 @@ import hashlib
 import base64
 import numpy as np
 
-PrevHash='0000000000000000000000000000000000000000000000000000000000000000'[::-1]
-Contents="1"
-Length=np.uint32(1)
+#works for http://6857coin.csail.mit.edu/block/0000005a67fc720f55953ee20e47b96c47b751bb0214c2a6d4593b066ecc3738
+
+PrevHash='00000099e3f4ca4995c4558b32de66203a323c8aea77a273c86c7ee029ac7b4b'
+Contents='32'.encode('hex')
+Length="{0:0{1}x}".format(2,8)
 difficulty=24
 
-Nonce=np.uint64(15610264)
+
 
 def try_hash(Nonce):
     Hash = hashlib.sha256()
     Hash.update(PrevHash.decode("hex"))
-    Hash.update(Contents)
-    Hash.update(Nonce)
-    Hash.update(Length)
-    b = Hash.digest()[::-1].encode("hex")[0:difficulty]
+    Hash.update(Contents.decode("hex"))
+    Hash.update(Nonce.decode("hex"))
+    Hash.update(Length.decode("hex"))
+    b = Hash.digest().encode("hex")[0:difficulty]
     ##    print Nonce
-##    print Hash.digest().encode("hex")
-##    print Hash.digest()[::-1].encode('hex_codec')
-##    print b
+    print Hash.digest().encode("hex")
+    print b
     if (b=='0'*24):
         return True
     else:
@@ -34,4 +35,5 @@ def loop():
         i+=1
  
 
-loop()
+#loop()
+try_hash("{0:0{1}x}".format(15610264,16))
